6f3be7dc2dc71a1ae210416911c5cb671365ff20
Wendrew <wendrew@pop-os.localdomain>
2019-01-17 11:14:42 -0200

    implementando o query_set para a app de enderecos

--- endereco/api/viewsets.py
+++ endereco/api/viewsets.py
@@ -4,5 +4,7 @@ from endereco.models import Endereco
 from endereco.api.serializers import EnderecoSerializer
 
 class EnderecoViewset(ModelViewSet):
-    queryset = Endereco.objects.all()
     serializer_class = EnderecoSerializer
+
+    def get_queryset(self):
+        return Endereco.objects.all()
