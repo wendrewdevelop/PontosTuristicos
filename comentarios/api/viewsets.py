ac238435c7f3fedb6f6129c8298b740815148c03
Wendrew <wendrew@pop-os.localdomain>
2019-01-17 11:18:46 -0200

    implementando get_query na app de comentarios

--- comentarios/api/viewsets.py
+++ comentarios/api/viewsets.py
@@ -4,5 +4,7 @@ from comentarios.models import Comentario
 from comentarios.api.serializers import ComentarioSerializer
 
 class ComentarioViewset(ModelViewSet):
-    queryset = Comentario.objects.all()
     serializer_class = ComentarioSerializer
+
+    def get_queryset(self):
+        return Comentario.objects.all()
